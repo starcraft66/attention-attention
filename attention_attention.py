d9baa06918c4b98e98a049ee147eacf99a905502
Tristan Gosselin-Hane <starcraft66@gmail.com>
2020-11-24 13:30:02 -0500

    Correct license text

--- attention_attention.py
+++ attention_attention.py
@@ -1,18 +1,18 @@
 # Copyright (C) Tristan Gosselin-Hane
 # This file is part of attention-attention <https://github.com/starcraft66/attention-attention>.
 #
-# dogtag is free software: you can redistribute it and/or modify
+# attention-attention is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# dogtag is distributed in the hope that it will be useful,
+# attention-attention is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with dogtag.  If not, see <http://www.gnu.org/licenses/>.
+# along with attention-attention.  If not, see <http://www.gnu.org/licenses/>.
 
 
 import discord
